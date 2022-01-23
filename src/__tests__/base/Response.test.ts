60ddc4e5bcb9bceca1c5c67095e048cb5485f4c8
beanc16 <bean.christopher@outlook.com>
2022-01-23 10:35:29 -0500

    Add tests for Response

---
+++ src/__tests__/base/Response.test.ts
@@ -0,0 +1,102 @@
+import { Response } from '../../base/Response';
+
+
+
+// Helpers
+const defaultResponse = {
+    statusCode: 200,
+};
+
+function getResult(params: object)
+{
+    return Object.assign({}, defaultResponse, params);
+}
+
+
+
+// Tests
+test("Response - Empty Params", function ()
+{
+    const params = {
+    };
+
+    const response = new Response(params);
+    const result = getResult(params);
+
+    expect(response).toEqual(result);
+});
+
+
+test("Response - statusCode=400", function ()
+{
+    const params = {
+        statusCode: 400,
+    };
+
+    const response = new Response(params);
+    const result = getResult(params);
+
+    expect(response).toEqual(result);
+});
+
+
+test(`Response - message="hi"`, function ()
+{
+    const params = {
+        message: "hi",
+    };
+
+    const response = new Response(params);
+    const result = getResult(params);
+
+    expect(response).toEqual(result);
+});
+
+
+test(`Response - data={ foo: "bar" }`, function ()
+{
+    const params = {
+        data: {
+            foo: "bar",
+        }
+    };
+
+    const response = new Response(params);
+    const result = getResult(params);
+
+    expect(response).toEqual(result);
+});
+
+
+test(`Response - Error`, function ()
+{
+    const params = {
+        error: new Error("I'm a big scary error!"),
+    };
+
+    const response = new Response(params);
+    const result = getResult(params);
+
+    expect(response).toEqual(result);
+});
+
+
+test(`Response - Custom Error`, function ()
+{
+    class BigScaryError extends Error
+    {
+        constructor()
+        {
+            super("I'm a big scary error!");
+        }
+    }
+    
+    const params = {
+        error: new BigScaryError(),
+    };
+
+    const response = new Response(params);
+    const result = getResult(params);
+
+    expect(response).toEqual(result);
+});
