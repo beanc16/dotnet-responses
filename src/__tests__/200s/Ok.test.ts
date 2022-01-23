684f8fc7afff432065554b5113dcfec71e242187
beanc16 <bean.christopher@outlook.com>
2022-01-23 15:45:40 -0500

    Add ok unit tests for functions

--- src/__tests__/200s/Ok.test.ts
+++ src/__tests__/200s/Ok.test.ts
@@ -1,6 +1,10 @@
 import { Ok } from "../../200s/Ok";
 import { response as expressRes } from "express";
 import { ExpressResponse } from "../../base";
+import {
+    ResDoesNotExistError,
+    ResFunctionDoesNotExistError,
+ } from "../../base/errors";
 
 
 
@@ -35,7 +39,7 @@ function getExpressResponse() : ExpressResponse
 
 
 
-// Tests
+// Constructor
 test("Ok - Empty Params", function ()
 {
     const response = new Ok();
@@ -88,6 +92,7 @@ test(`Ok - data={ foo: "bar" }`, function ()
 });
 
 
+// Object functions
 test(`Ok - send (undefined res)`, function ()
 {
     const params = {
@@ -96,7 +101,19 @@ test(`Ok - send (undefined res)`, function ()
 
     const response = new Ok(params);
 
-    expect(() => response.send()).toThrow();
+    expect(() => response.send()).toThrowError(ResDoesNotExistError);
+});
+
+
+test(`Ok - send (null res)`, function ()
+{
+    const params = {
+        res: null,
+    };
+
+    const response = new Ok(params);
+
+    expect(() => response.send()).toThrowError(ResDoesNotExistError);
 });
 
 
@@ -113,6 +130,101 @@ test(`Ok - send (valid res)`, function ()
 });
 
 
+test(`Ok - send (undefined function)`, function ()
+{
+    const params = {
+        res: getExpressResponse(),
+    };
+    // Remove send function to trigger error
+    params.res.send = undefined;
+
+    const response = new Ok(params);
+
+    expect(() => response.send()).toThrowError(ResFunctionDoesNotExistError);
+});
+
+
+test(`Ok - json`, function ()
+{
+    const params = {
+        res: getExpressResponse(),
+    };
+
+    const response = new Ok(params);
+
+    // Express throws TypeError for non-live responses
+    expect(() => response.json()).toThrowError(TypeError);
+});
+
+
+test(`Ok - jsonp`, function ()
+{
+    const params = {
+        res: getExpressResponse(),
+    };
+
+    const response = new Ok(params);
+
+    // Express throws TypeError for non-live responses
+    expect(() => response.jsonp()).toThrowError(TypeError);
+});
+
+
+test(`Ok - sendStatus`, function ()
+{
+    const params = {
+        res: getExpressResponse(),
+    };
+
+    const response = new Ok(params);
+
+    // Express throws TypeError for non-live responses
+    expect(() => response.sendStatus()).toThrowError(TypeError);
+});
+
+
+test(`Ok - status (no input)`, function ()
+{
+    const params = {
+        res: getExpressResponse(),
+    };
+
+    const response = new Ok(params);
+    const result = response.status();
+
+    expect(params.res).toEqual(result);
+});
+
+
+test(`Ok - status (statusCode=200)`, function ()
+{
+    const params = {
+        res: getExpressResponse(),
+    };
+
+    const response = new Ok(params);
+    const result = response.status(200);
+
+    expect(params.res).toEqual(result);
+});
+
+
+test(`Ok - status (statusCode=400)`, function ()
+{
+    const params = {
+        res: getExpressResponse(),
+    };
+
+    const response = new Ok(params);
+    const result = response.status(400);
+
+    expect(params.res).toEqual(result);
+});
+
+// TODO: end
+
+
+// Static functions
 test(`Ok static - send (valid res)`, function ()
 {
     const params = {
@@ -122,3 +234,76 @@ test(`Ok static - send (valid res)`, function ()
     // Express throws TypeError for non-live responses
     expect(() => Ok.send(params)).toThrowError(TypeError);
 });
+
+
+test(`Ok static - json (valid res)`, function ()
+{
+    const params = {
+        res: getExpressResponse(),
+    };
+
+    // Express throws TypeError for non-live responses
+    expect(() => Ok.json(params)).toThrowError(TypeError);
+});
+
+
+test(`Ok static - jsonp (valid res)`, function ()
+{
+    const params = {
+        res: getExpressResponse(),
+    };
+
+    // Express throws TypeError for non-live responses
+    expect(() => Ok.jsonp(params)).toThrowError(TypeError);
+});
+
+
+test(`Ok static - sendStatus (valid res)`, function ()
+{
+    const params = {
+        res: getExpressResponse(),
+    };
+
+    // Express throws TypeError for non-live responses
+    expect(() => Ok.sendStatus(params)).toThrowError(TypeError);
+});
+
+
+test(`Ok static - status (no input)`, function ()
+{
+    const params = {
+        res: getExpressResponse(),
+    };
+
+    const result = Ok.status(params);
+
+    expect(params.res).toEqual(result);
+});
+
+
+test(`Ok - status (statusCode=200)`, function ()
+{
+    const params = {
+        res: getExpressResponse(),
+        statusCode: 200,
+    };
+
+    const result = Ok.status(params);
+
+    expect(params.res).toEqual(result);
+});
+
+
+test(`Ok - status (statusCode=400)`, function ()
+{
+    const params = {
+        res: getExpressResponse(),
+        statusCode: 400,
+    };
+
+    const result = Ok.status(params);
+
+    expect(params.res).toEqual(result);
+});
+
+// TODO: Static end
