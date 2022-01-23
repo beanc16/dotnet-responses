32dabd5e8335afcf66c351a5a46beb1ea800fae1
beanc16 <bean.christopher@outlook.com>
2022-01-23 14:33:37 -0500

    Fix bug with Ok

--- src/200s/Ok.ts
+++ src/200s/Ok.ts
@@ -1,19 +1,30 @@
-import { ResponseWithStatus, ResponseParamsWithNoStatus } from "../base";
+import {
+    ResponseWithStatus,
+    ResponseParamsWithNoStatus,
+} from "../base";
 
 
 
 export class Ok extends ResponseWithStatus
 {
+    protected static override defaultParams = {
+        res: undefined,
+        statusCode: 200,
+        message: "Ok",
+        data: null,
+        error: null,
+    };
+
     constructor({
         res,
-        message = "Ok",
+        message = Ok.defaultParams.message,
         data,
         error,
-    }: ResponseParamsWithNoStatus)
+    }: ResponseParamsWithNoStatus = Ok.defaultParams)
     {
         super({
             res,
-            statusCode: 200,
+            statusCode: Ok.defaultParams.statusCode,
             message,
             data,
             error,
